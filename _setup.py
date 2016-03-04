fa9c5895d53136dbc8159a42bdfe11981640cbea
aml <aml@opencascade.com>
2016-03-04 20:43:18 +0300

    Setup script is modified to generate all binaries.

---
+++ _setup.py
@@ -0,0 +1,9 @@
+from cx_Freeze import setup, Executable
+
+
+setup(
+    name = "UtPy",
+    version = "0.01",
+    description = "Useful scripts which are written in python language",
+    executables = [Executable("DeltaPrepare.py"), Executable("CodeCount.py"),]
+)
--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-
-setup(
-    name = "DeltaPrepare" ,
-    version = "0.1" ,
-    description = " " ,
-    executables = [Executable("DeltaPrepare.py")]
-)
